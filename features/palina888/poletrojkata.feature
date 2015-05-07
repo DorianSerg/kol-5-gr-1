bc093b501b1ac33081cc1cb9682fb8c4a0860a5c
palina888 <palina888@o2.pl>
2015-05-07 12:06:43 +0200

    palina888:poletrojkata

--- features/palina888/poletrojkata.feature
+++ features/palina888/poletrojkata.feature
@@ -3,7 +3,7 @@ Feature: Obliczenia
   Scenario: Poletrojkata a * b * 0.5
     Given I am on homepage
     When I follow "Kalkulator by palina888"
-    And I fill in "a" with "8"
-    And I fill in "b" with "5"
+    And I fill in "a" with "4"
+    And I fill in "b" with "4"
     And I press "Oblicz"
-    Then I should see "Wynik wynosi 20"
+    Then I should see "Wynik wynosi 8"
--- spec/palina888/Tools/PoletrojkataSpec.php
+++ spec/palina888/Tools/PoletrojkataSpec.php
@@ -11,4 +11,13 @@ class PoletrojkataSpec extends ObjectBehavior
     {
         $this->shouldHaveType('palina888\Tools\Poletrojkata');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+    function it_should_calculate_multiply()
+    {
+    	$this->setA(4)->setB(4)->multiply()->shouldReturn(8.0);
+    }
 }
