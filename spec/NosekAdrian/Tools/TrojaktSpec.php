f7a94e1158ff1e91f94bcfe4e831063b4d4e5d25
NosekAdrian <adrian.nosek@windowslive.com>
2015-05-07 11:53:59 +0200

    NosekAdrian/Trojkat: implementacja metod

--- spec/NosekAdrian/Tools/TrojaktSpec.php
+++ spec/NosekAdrian/Tools/TrojaktSpec.php
@@ -11,4 +11,15 @@ class TrojaktSpec extends ObjectBehavior
     {
         $this->shouldHaveType('NosekAdrian\Tools\Trojakt');
     }
+    
+    function it_should_have_setter_and_getter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+    
+    function it_should_calculate_pole()
+    {
+        $this->setA(5)->setB(10)->poletrojkata()->shouldReturn(25);
+    }
 }
--- src/NosekAdrian/Tools/Trojakt.php
+++ src/NosekAdrian/Tools/Trojakt.php
@@ -4,4 +4,33 @@ namespace NosekAdrian\Tools;
 
 class Trojakt
 {
+    private $a;
+    private $b;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+        return $this;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function poletrojkata()
+    {
+        return $this->a * $this->b * 1/2;
+    }
 }
