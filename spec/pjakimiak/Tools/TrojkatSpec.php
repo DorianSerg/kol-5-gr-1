ed613b511789fba41761d273fcb7ade142fac25f
pjakimiak <pawel.jakimiak@o2.pl>
2015-05-07 09:35:52 +0000

    trojkat pjakimiak: implementacja metod

--- spec/pjakimiak/Tools/TrojkatSpec.php
+++ spec/pjakimiak/Tools/TrojkatSpec.php
@@ -11,4 +11,10 @@ class TrojkatSpec extends ObjectBehavior
     {
         $this->shouldHaveType('pjakimiak\Tools\Trojkat');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
 }
--- src/pjakimiak/Tools/Trojkat.php
+++ src/pjakimiak/Tools/Trojkat.php
@@ -4,4 +4,55 @@ namespace pjakimiak\Tools;
 
 class Trojkat
 {
+
+    private $a;
+    private $b;
+    private $c;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+    
+    public function setC($c)
+    {
+        $this->c = $c;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+    
+    public function getC()
+    {
+        return $this->c;
+    }
+    
+    function it_should_calculate_pole()
+    {
+    $this->setA(1)->setB(2)->setC(0.5)->pole()->shouldReturn(1.0);
+    }
+    
+    public function pole()
+    {
+    return $this->a * $this->b * $this->c;
+    }
+
 }
