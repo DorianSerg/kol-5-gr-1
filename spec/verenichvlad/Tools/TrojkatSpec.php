68a6e3cc609ae5709db33cb683faf46a19b0cc50
verenichvlad <verenichvlad@gmail.com>
2015-05-21 11:42:39 +0200

    Kolos done

--- spec/verenichvlad/Tools/TrojkatSpec.php
+++ spec/verenichvlad/Tools/TrojkatSpec.php
@@ -11,4 +11,15 @@ class TrojkatSpec extends ObjectBehavior
     {
         $this->shouldHaveType('verenichvlad\Tools\Trojkat');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+
+    function it_should_calculate_pole()
+    {
+        $this->setA(5)->setB(10)->multiply()->shouldReturn(25.0);
+    }
 }
