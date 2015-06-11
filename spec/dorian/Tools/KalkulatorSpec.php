a42564561055e56ffc76a55314aed45e8720d734
DorianSerg <dorian.sergiel@gmail.com>
2015-06-11 12:21:48 +0200

    dorian/kalkulatir - koniec

--- spec/dorian/Tools/KalkulatorSpec.php
+++ spec/dorian/Tools/KalkulatorSpec.php
@@ -11,4 +11,16 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('dorian\Tools\Kalkulator');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(2)->getA()->shouldReturn(2);
+        $this->setB(3)->getB()->shouldReturn(3);
+        $this->setH(4)->getH()->shouldReturn(4);
+    }
+    
+    function it_should_calculate_sum()
+    {
+        $this->setA(2)->setB(3)->setH(4)->sum()->shouldReturn(24);
+    }
 }
--- src/dorian/Tools/Kalkulator.php
+++ src/dorian/Tools/Kalkulator.php
@@ -4,4 +4,49 @@ namespace dorian\Tools;
 
 class Kalkulator
 {
+
+    private $a;
+    private $b;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+    
+    public function setH($h)
+    {
+        $this->h = $h;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+    
+    public function getH()
+    {
+        return $this->h;
+    }
+
+
+    public function sum()
+    {
+        return $this->a * $this->b * $this->h;
+    }
 }
